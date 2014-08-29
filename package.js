c1159126bae94306b31575762c02a5b0a1e4ac50
zimme <zimme@users.noreply.github.com>
2014-08-29 18:25:54 +0200

    Add bootstrap-hover-dropdown.js in package.js

--- package.js
+++ package.js
@@ -6,6 +6,6 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.addFiles('meteor-bootstrap-hover-dropdown.js');
   api.versionsFrom('0.9.0.1');
+  api.addFiles('bootstrap-hover-dropdown/bootstrap-hover-dropdown.js');
 });
